233fb71de07f7c8ee72ad045fc20b453782383b0
codypage <100495823+codypage@users.noreply.github.com>
2022-05-19 09:35:43 -0500

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+// We'll write out most of our code for this application here.
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Store posts in memory, so create an empty object
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
